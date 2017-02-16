d90f2181edfa3a7e181ce1a06b8c518ea02fb561
Nick Prühs <dev@npruehs.de>
2017-02-16 11:55:18 +0100

    Add login controller.

---
+++ GameBackend/WebService/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using LobbyActor.Interfaces;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebService.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        // GET api/login/5
+        [HttpGet("{id}")]
+        public async Task<string> Get(string id)
+        {
+            var lobbyActor = ActorProxy.Create<ILobbyActor>(new ActorId(0));
+            var count = await lobbyActor.GetCountAsync(new System.Threading.CancellationToken());
+            return count.ToString();
+        }
+    }
+}
